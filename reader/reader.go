6cf1a39856df2d024e9b3241bcdbfafb9fc7f3d1
maik <estevan.maik@gmail.com>
2023-11-21 16:11:52 -0300

    feat: defines unmarshalJson func

--- reader/reader.go
+++ reader/reader.go
@@ -1,6 +1,7 @@
 package reader
 
 import (
+	"encoding/json"
 	"fmt"
 	"mittere/customerror"
 	"os"
@@ -11,10 +12,34 @@ var (
 	supportedExtensions []string
 )
 
+// define types in file
+type Request struct {
+	Url     string          `json:"url"`
+	Method  string          `json:"method"`
+	Headers json.RawMessage `json:"headers"`
+	Data    json.RawMessage `json:"data"`
+}
+
+func unmarshalRequest(filepath string) {
+	content, err := os.ReadFile(filepath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	request := Request{}
+
+	err2 := json.Unmarshal(content, &request)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	fmt.Printf("%s \n", request)
+}
+
 func Read(filePath string) string {
 	fileExt := filepath.Ext(filePath)
 
 	// fmt.Println(fileExt)
+	unmarshalRequest(filePath)
 
 	if fileExt != ".json" {
 		fmt.Printf("Unsupported file type")
