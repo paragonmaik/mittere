62fff6265e02da1518cd883e16d1c819bcbb07d0
maik <estevan.maik@gmail.com>
2023-11-27 13:52:29 -0300

    test: adds unit test to the Read func

---
+++ reader/reader_test.go
@@ -0,0 +1,77 @@
+package reader
+
+import (
+	// "encoding/json"
+	// "os"
+	// "bytes"
+	// "fmt"
+
+	"errors"
+	"testing"
+)
+
+type request struct {
+	Url     string
+	Method  string
+	Data    string
+	Headers string
+}
+
+func TestReader(t *testing.T) {
+	testCases := []struct {
+		name        string
+		filepath    string
+		expected    request
+		expectedErr error
+	}{{
+		name:     "test 1",
+		filepath: "../testdata/test2.json",
+		expected: request{
+			Url:    "https://jsonplaceholder.typicode.com/posts/1",
+			Method: "get",
+		}},
+		{
+			name:     "test 2",
+			filepath: "../testdata/test2.yaml",
+			expected: request{
+				Url:    "https://jsonplaceholder.typicode.com/posts/1",
+				Method: "get",
+			}},
+		{
+			name:        "test 3",
+			filepath:    "../testdata/testErr.txt",
+			expectedErr: &readErr{step: "read file"},
+		},
+		{
+			name:        "test 4",
+			filepath:    "../testdata/nonExistantFile.json",
+			expectedErr: &readErr{step: "read file"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			req, err := Read(testCase.filepath)
+			if testCase.expectedErr != nil {
+				if err == nil {
+					t.Errorf("Expected error; received none")
+				}
+
+				if !errors.Is(err, testCase.expectedErr) {
+					t.Errorf("Expected error: %q, received: %q",
+						testCase.expectedErr, err)
+				}
+			}
+
+			if req.Url != testCase.expected.Url {
+				t.Errorf("Expected out: %s, received %s", req.Url,
+					testCase.expected.Url)
+			}
+
+			if req.Method != testCase.expected.Method {
+				t.Errorf("Expected out: %s, received %s", req.Method,
+					testCase.expected.Method)
+			}
+		})
+	}
+}
