968d2bf332c4681f000b2b2427c73cd08e4a31ed
maik <estevan.maik@gmail.com>
2023-11-28 16:53:42 -0300

    feat: adds custom errors package

---
+++ errs/errs.go
@@ -0,0 +1,34 @@
+package errs
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidExt = errors.New("unsupported file extension")
+)
+
+type ReadErr struct {
+	Step  string
+	Msg   string
+	Cause error
+}
+
+func (s *ReadErr) Error() string {
+	return fmt.Sprintf("Step: %q: %s: Cause: %v", s.Step,
+		s.Msg, s.Cause)
+}
+
+func (s *ReadErr) Is(target error) bool {
+	t, ok := target.(*ReadErr)
+	if !ok {
+		return false
+	}
+
+	return t.Step == s.Step
+}
+
+func (s *ReadErr) Unwrap() error {
+	return s.Cause
+}
